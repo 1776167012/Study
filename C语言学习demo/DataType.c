8f3154547f3c0ecd20d32ce1f54510f18836b650
严家豆 <57281454+1776167012@users.noreply.github.com>
2021-06-10 10:16:38 +0800

    Add files via upload

---
+++ C语言学习demo/DataType.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("size of char is:%d\n",sizeof(char));
+	printf("size of short is:%d\n",sizeof(short));
+	printf("size of int is:%d\n",sizeof(int));
+	printf("size of short int is:%d\n",sizeof(short int));
+	printf("size of long int is:%d\n",sizeof(long int));
+	printf("size of float is:%d\n",sizeof(float));
+	printf("size of double is:%d\n",sizeof(double));
+    printf("size of long long is:%d\n",sizeof(long long));
+	return 0;
+}
